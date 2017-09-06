ce9f16b92dc8fd7cecb1b2231b23db938dfb016a
james-ciclitira <jciclitira@gmail.com>
2017-09-06 13:15:29 +0100

    netlify form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends React.Component {
     return (
 <section id='Contact' className='mw8 db ph3 center mt6'>
 <h2 className='fw4'>Get in touch</h2>
-<form name="contact" action="Sent" netlify>
+<form name="contact" action="Sent" data-netlify="true">
 <input id="name" className="bg-transparent pv3 mv2 dib w-48" placeholder='Name' type="text" name="name"/>
 <input id="email" className="bg-transparent pv3 mv2 dib w-48 fr" placeholder='Email' type="email" name="email"/>
 <textarea id="comment" rows="3" className="db w-100 bg-transparent pt3 mv3" placeholder='Message' name="message"></textarea>
