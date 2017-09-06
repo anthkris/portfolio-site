78ffa303529b06ae83f306b0c665f8062ac58422
james-ciclitira <jciclitira@gmail.com>
2017-09-06 14:05:55 +0100

    netlify thank you

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ class Contact extends React.Component {
 
 <section className='mw8 db ph3 center mt6'>
 <h2 className='fw4 mb5'>Get in touch</h2>
-<form name="contact" action="/Sent" data-netlify="true">
+<form name="contact" action="thank-you" data-netlify="true">
 <label className='dib w-50-ns w-100'>Name:
 <input className="bg-transparent db mt3 w-80 pointer" type="text" name="name"/>
 </label>
