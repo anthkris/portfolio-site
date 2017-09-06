d9c3d37c8bdb50db79a2b5ca53961374fd0dd86a
james-ciclitira <jciclitira@gmail.com>
2017-09-06 14:25:07 +0100

    form clean up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,20 +6,28 @@ class Contact extends React.Component {
     return (
 <section className='mw8 db ph3 center mt6'>
 <h2 className='fw4 mb5'>Get in touch</h2>
-<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="thank-you">
-<label className='dib w-50-ns w-100'>Name:
-<input className="bg-transparent db mt3 w-80-ns w-100 pointer" type="text" name="name"/>
-</label>
-<label className='dib w-50-ns w-100 mt4 mt0-ns'>Email:
-<input className="bg-transparent db mt3 w-80-ns w-100 pointer" type="email" name="email"/>
-</label>
-<label className='db w-100 pt4 mv3'>Message
-<textarea rows="3" className="db w-100 bg-transparent pointer" name="message">
-</textarea>
-</label>
-<button className='f6 grow no-underline bn ph4 pv3 mt3 dib bg-black white mt5 shadow-4'
-style={{borderRadius:'6'}}
-type="submit">Send</button>
+<form
+    name="contact"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    action="thank-you"
+    >
+    <label className='dib w-50-ns w-100'>Name:
+    <input className="bg-transparent db mt3 w-80-ns w-100 pointer" type="text" name="name"/>
+    </label>
+
+    <label className='dib w-50-ns w-100 mt4 mt0-ns'>Email:
+    <input className="bg-transparent db mt3 w-80-ns w-100 pointer" type="email" name="email"/>
+    </label>
+
+    <label className='db w-100 pt4 mv3'>Message
+    <textarea rows="3" className="db w-100 bg-transparent pointer" name="message"/>
+    </label>
+
+    <button className='f6 grow no-underline bn ph4 pv3 mt3 dib bg-black white mt5 shadow-4'
+    style={{borderRadius:'6'}}
+    type="submit">Send</button>
 </form>
 </section>
     )
