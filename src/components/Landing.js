0511d2e18760e12ebab3a3b1e85b548b9c9b0292
james-ciclitira <jciclitira@gmail.com>
2017-09-10 09:53:27 +0100

    landing layout + copy

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,13 +9,10 @@ class Landing extends React.Component {
 <div>
   <section className='overflow-hidden' >
       <div className='mw8 center db pt2'>
-          <div className="dib v-top w-50-ns w-100 mt4-ns pa0-ns pa3 tl-ns tc">
-          <h1 className='ma0 f2-ns f3 dib fw2 pt4 pt0' style={{lineHeight: '1.3'}}>
-          Freelance Product Designer. Previously at
-          <a target='blank' className='no-underline dim' href='https://www.yunojuno.com/'> YunoJuno</a>,
-          <a target='blank' className='no-underline dim' href='https://www.sugges.tv/'> Suggestv</a> &
-          <a target='blank' className='no-underline dim' href='http://crowdscores.com/'> Crowdscores.</a>
-          </h1>
+          <div className="dib v-top w-50-ns w-100 mt4-ns pa0-ns pa3 mw6 tl-ns tc ">
+          <h1 className='fw5 f1 mb2'>Hello!</h1>
+          <h2 className='fw3' style={{lineHeight: '1.5'}}>I'm James Ciclitira, a freelance product designer based in London. Specialising in native & web apps.
+          </h2>
           <Availability/>
           <a className="f6 grow no-underline ph4 pv3 mt4 dib bg-black white mt5 shadow-4" style={{borderRadius:'6px'}} href="mailto:jciclitira@gmail.com">Get in touch</a>
           <Social/>
