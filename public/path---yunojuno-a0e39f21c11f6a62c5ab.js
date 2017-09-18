d9ecf6f195fdd2ac1c584d76c4a620ad4f7ed24a
james-ciclitira <jciclitira@gmail.com>
2017-09-18 16:57:05 +0100

    img

---
+++ public/path---yunojuno-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x72d2e4e6d0cac80],{"./node_modules/json-loader/index.js!./.cache/json/yunojuno.json":function(o,n){o.exports={pathContext:{}}}});
+//# sourceMappingURL=path---yunojuno-a0e39f21c11f6a62c5ab.js.map
