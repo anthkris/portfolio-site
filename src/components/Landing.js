4b11d3d9c425e77be50a9cd6a0099d71a1886f0c
james-ciclitira <jciclitira@gmail.com>
2017-10-10 07:37:25 +0100

    copy changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Availability from './Availability'
 import Social from './Social'
 
 
@@ -11,7 +10,7 @@ class Landing extends React.Component {
       <div className='mw8 center db pt2'>
           <div className="dib v-top w-50-ns w-100 mt4-ns pa0-ns pa3 mw6 tl-ns tc ">
           <h1 className='fw5 f1 mb2'>Hello!</h1>
-          <h2 className='fw3' style={{lineHeight: '1.5'}}>I'm James Ciclitira, a digital product designer based in London in the process of moving to Berlin. Specialising in native & web apps.
+          <h2 className='fw3' style={{lineHeight: '1.5'}}>I'm James Ciclitira, a Digital Product Designer based in London in the process of moving to Berlin. Specialising in the design of native & web applications.
           </h2>
           <a className="f6 grow no-underline ph4 pv3 mt4 dib bg-black white mt5 shadow-4" style={{borderRadius:'6px'}} href="mailto:jciclitira@gmail.com">Get in touch</a>
           <Social/>
