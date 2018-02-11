814d2c66dad21e4c019baa6d186c0f3b3bbaa3f3
james-ciclitira <jciclitira@gmail.com>
2018-02-11 13:25:37 +0100

    font

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,10 +5,9 @@ class Landing extends React.Component {
   render() {
     return (
   <section >
-          <div className="dib v-top w-80-ns w-100 mt4-ns pa0 tl-ns tc vh-100 pt3">
-          <p className='f7 secondary-text ttu'>James Ciclitira - Product Designer</p>
-          <h1 className='f2-ns f3' style={{lineHeight: '1.4'}}>Hello! I'm a Product Designer based in Berlin, designing digital experiences using user-centred design principles to create meaningful user experiences.</h1>
-          <a className="f6 grow no-underline ph4 pv3 dib neon white mt4" style={{borderRadius:'6px'}} href="#form">Get in touch</a>
+          <div className="dib v-top w-80-ns w-100 mt4-ns pa0 tl-ns tc vh-100">
+          <h2 className='f2-ns pt2 f3' style={{lineHeight: '1.4'}}>Hello! <br /> I'm a Product Designer based in Berlin, creating meaningful digital experiences with a focus on user-centred design.</h2>
+          <a className="f6 grow no-underline ph4 pv3 dib neon white mt3" style={{borderRadius:'6px'}} href="#form">Get in touch</a>
           <Social/>
           </div>
 </section>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,10 +8,10 @@ import './index.css'
 const Header = () =>
 
    <nav className='mw8 db center' >
-        <div className="dt-l w-100 border-box pa3">
-          <Link className="db dtc-l v-mid link hover-red tc tl-l mb2 mb0-l f4 fw3" to="/" title="Home">James Ciclitira</Link>
+        <div className="dt-l w-100 border-box pa3 pt4">
+          <Link className="db dtc-l v-mid link hover-neon tc tl-l mb0-l mb2 f6 ttu" to="/" title="Home">James Ciclitira</Link>
           <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-            <a className="link dib mr3-l mr0 hover-red fw3" target='blank' href="https://jimmmy.design/">Digital Résumé</a>
+            <a className="link dib mr3-l mr0 hover-neon f6 ttu" target='blank' href="https://jimmmy.design/">Digital Résumé</a>
           </div>
         </div>
       </nav>
