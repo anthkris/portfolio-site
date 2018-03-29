c8741bddf9b1c6bde4a60d68f68471016168c290
james-ciclitira <jciclitira@gmail.com>
2018-03-29 20:09:50 +0200

    header l

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,11 +4,13 @@ import Social from './Social'
 class Landing extends React.Component {
   render() {
     return (
-  <section>
-          <div className="dib v-top mw7 pr7-ns pr0 mt5-ns pa0 tl-ns tc vh-100">
-          <h1>Digital Product and Web Designer in Berlin</h1>
-          <h3 className='mt4 fw3' style={{lineHeight: '1.8'}}>Hi there! My name is James Ciclitira. I creating meaningful digital experiences with a focus on user-centred design.</h3>
-          <a className="f6 grow no-underline ph4 pv3 dib neon white mt3" style={{borderRadius:'6px'}} href="#form">Get in touch</a>
+  <section >
+          <div className=' absolute vh-100 top-0 left-0' style={{backgroundImage: 'url(images/bg.svg)', zIndex:'-99999'}}>
+          </div>
+          <div className="dib v-top pr7-ns pr0 pa0 tl-ns tc vh-100">
+          <h1 className='f-subheadline-l mt5'>Digital Product Designer</h1>
+          <h3 className='mt4 fw3' style={{lineHeight: '1.8'}}>Hi there! I'm James, I creating meaningful digital experiences with a focus on user-centred design.</h3>
+          <a className="f6 grow no-underline ph4 pv3 dib white neon  mt3" style={{borderRadius:'6px'}} href="#form">Get in touch</a>
           <Social/>
           </div>
 </section>
