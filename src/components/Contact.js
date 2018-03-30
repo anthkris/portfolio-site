a9f23f25b2f5070b2bc44f7658e19eed2e3828db
james-ciclitira <jciclitira@gmail.com>
2018-03-30 13:28:51 +0200

    bg-transparent

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,8 +9,8 @@ class Contact extends React.Component {
   <form name="contact" method="post" action="/thanks/" data-netlify="true" data-netlify-honeypot="bot-field">
   <input type="hidden" name="form-name" value="contact" />
     <input type="hidden" name="form-name" value="contact"/><p hidden><label>Don’t fill this out:<input name="bot-field"/></label></p>
-    <input className='w-40-ns mb4 mb0-ns w-100' type='name' name='email' placeholder='Name' required/>
-    <input className='w-40-ns w-100 ml3-ns ml0' type='email' name='email' placeholder='Email' required/>
+    <input className='w-40-ns mb4 mb0-ns w-100 bg-transparent' type='name' name='email' placeholder='Name' required/>
+    <input className='w-40-ns w-100 ml3-ns ml0 bg-transparent' type='email' name='email' placeholder='Email' required/>
     <textarea rows="3" placeholder='Message' className="db w-100 mt4 bg-transparent pointer" name="message" required/>
 <button className='f6 grow no-underline bn ph4 pv3 mt3 dib neon white mt5 shadow-4 pointer'
     style={{borderRadius:'6px'}}
