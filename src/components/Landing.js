084a49a97d5a2905fd81b5790027df13e89900b2
james-ciclitira <jciclitira@gmail.com>
2018-03-30 13:19:07 +0200

    header update

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,13 +5,15 @@ class Landing extends React.Component {
   render() {
     return (
   <section >
-          <div className=' absolute vh-100 top-0 left-0' style={{backgroundImage: 'url(images/bg.svg)', zIndex:'-99999'}}>
-          </div>
-          <div className="dib v-top pr7-ns pr0 pa0 tl-ns tc vh-100">
-          <h1 className='f-subheadline-l mt5'>Digital Product Designer</h1>
-          <h3 className='mt4 fw3' style={{lineHeight: '1.8'}}>Hi there! I'm James, I creating meaningful digital experiences with a focus on user-centred design.</h3>
-          <a className="f6 grow no-underline ph4 pv3 dib white neon  mt3" style={{borderRadius:'6px'}} href="#form">Get in touch</a>
+          <img className='absolute top-0 right-0 db-ns dn' alt='bg-img' src='images/bg.svg' style={{zIndex:'-99999', height:'60vh' }}/>
+          <div className="dib v-top pr0 pa0 tl-ns tc vh-100">
+          <div className='w-60-ns w-100 dib mt4 v-top'>
+          <h1 className='f-subheadline-l mt5-ns '>Digital Product Designer</h1>
+          <h3 className='mt4 fw1 font-1 pr5-ns pr0' style={{lineHeight: '1.8' }}>Hi I'm James a product desinger based in berlin, creating meaningful digital experiences with a focus on user-centred design.</h3>
           <Social/>
+          </div>
+    <div className='dib-ns dn w-40 mt5'><img className=' mr2 mt4' alt='iphone-header' src='images/iphone.png'/></div>
+
           </div>
 </section>
     )
