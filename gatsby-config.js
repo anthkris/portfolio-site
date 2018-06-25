f1502645fe69e563b5bcb1f982603712e8ff1518
anthkris <anthkris09@gmail.com>
2018-06-24 21:08:57 -0500

    fix: change source file system to look in pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages/posts`,
+        path: `${__dirname}/src/pages`,
         name: 'pages',
       },
     },
