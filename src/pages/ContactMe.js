e4e1085ee8eb88c05a9b895aee08e5cac1392af0
anthkris <anthkris09@gmail.com>
2018-07-03 22:11:03 -0500

    feat: Add contact form to contact page

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import Contact from '../components/Contact.js';
+import Social from '../components/Social.js';
 import '../styles/contact.scss';
 
 
 const ContactMe = () =>
-  <section id="contact-me">
-    <div className='center mw8 db ph3 pt3 pb5'>
-      <h1>Get at Me</h1>
-      <p>Use the TypeForm below to get in touch with me.</p>
-      <div className='contact-form'>
-      </div>
+<section id="contact-me">
+  <div className='center mw8 db ph3 pt3 pb5'>
+    <h1>Get at Me</h1>
+    <p>Use the form below to send me an email. Or get in touch with me here: kristin@knanthony.com</p>
+    <div className='contact-form'>
+      <Contact />
     </div>
-  </section>
+    <h2>I also Social</h2>
+    <p>You can also reach out on social media.</p>
+    <Social />
+  </div>
+</section>
 
 export default ContactMe;
