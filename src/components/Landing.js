e6bcecfe474b5df905d14e33c35406c72c5ea941
anthkris <anthkris09@gmail.com>
2018-07-03 20:56:51 -0500

    style: Style blog post margins and cta in landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,13 +8,13 @@ import '../styles/landing.scss';
 class Landing extends React.Component {
   render() {
     return (
-      <section className='center mw8 db ph3'>
+      <section id='landing' className='center mw8 db ph3'>
         <img className='absolute top-0 right-0 db-ns dn' alt='bg-img' src={bg} style={{zIndex:'-99999', height:'60vh' }}/>
         <div className="dib v-top pr0 pa0 tl-ns tc vh-100">
           <div className='w-50-ns w-100 dib mt4 v-top'>
             <h1 style={{letterSpacing:'-0.85px'}} className='f-subheadline-l mt5-ns'>Learning Experience Designer</h1>
-            <h2 className='mt4 fw1 font-1 pr5-ns pr0 about-me' style={{lineHeight: '1.8' }}>Hi I'm Kristin, a Learning Experience Designer. I help create meaningful, memorable, and motivational learning experiences with a focus on sustainable, accessible, user-centered design.</h2>
-            <Link to='/HowIWork' title='How I Work'>How I can help your business</Link>
+            <h2 className='mt4 mb4 fw1 font-1 pr5-ns pr0 about-me' style={{lineHeight: '1.8' }}>Hi I'm Kristin, a Learning Experience Designer. I help create meaningful, memorable, and motivational learning experiences with a focus on sustainable, accessible, user-centered design.</h2>
+            <Link className='hire-me' to='/HowIWork' title='How I Work'>Looking for a good LX Designer?</Link>
             <Social/>
           </div>
           <div className='dib-ns dn w-50 mt5'><img className=' mt4' alt='course and app mockups' src={mockups} /></div>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,7 +12,7 @@ const Header = () =>
     <div className="dt-l w-100 border-box pa3 pt4 ttu f6">
       <Link className="db dtc-l v-mid link hover-neon tc tl-l mb0-l mb2 " to="/" title="Home">K. Anthony</Link>
       <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-        <a className="link dib mr3 hover-black" href="/HowIWork">I Can Help</a>
+        <a className="link dib mr3 hover-black" href="/HowIWork">Hire Me</a>
         <a className="link dib mr3 hover-black" href="/ContactMe">Contact</a>
         <a className="link dib mr3 hover-black" href="/blog">Blog</a>
       </div>
