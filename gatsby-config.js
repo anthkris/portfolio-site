fb8dbf7eb7674dc53fea28435e0227150f0e6b05
anthkris <anthkris09@gmail.com>
2018-07-07 01:58:17 -0500

    feat: Add web manifest
    Install gatsby manifest plugin
    Add manifest to gatsby config
    
    Resolves: #15

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,6 +95,18 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-react-next`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "K. Anthony Portfolio",
+        short_name: "KAnthony",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#ea3257",
+        display: "minimal-ui",
+        icon: "src/pages/posts/featured/logoColor.png", // This path is relative to the root of the site.
+      },
+    },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`
   ],
